dd97b150980b0320e284a3d917a5bb0631813639
Deepanshu Arora <43300955+Deepansharora27@users.noreply.github.com>
2020-05-16 12:18:29 +0530

    Add files via upload

---
+++ Desktop/Coding Contests/LeetCodeThirdWeekTwelthProblem.cpp
@@ -0,0 +1,87 @@
+
+/*
+
+PROBLEM STATEMENT : 
+
+Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+
+Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+
+Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+
+ 
+
+Example 1:
+
+Input: [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3
+
+Example 2:
+
+Input: [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+
+Example 3:
+
+Input: [3,-1,2,-1]
+Output: 4
+Explanation: Subarray [2,-1,3] has maximum sum 2 + (-1) + 3 = 4
+
+Example 4:
+
+Input: [3,-2,2,-3]
+Output: 3
+Explanation: Subarray [3] and [3,-2,2] both have maximum sum 3
+
+Example 5:
+
+Input: [-2,-3,-1]
+Output: -1
+Explanation: Subarray [-1] has maximum sum -1
+
+
+*/
+
+class Solution {
+public:
+    
+    int KadaneAlgorithm(vector<int> input){
+        int max_sum = input[0]; 
+        int current_sum = max_sum ; 
+        
+        for(int i=1 ; i<input.size() ; i++){
+            current_sum = max(current_sum + input[i] , input[i]); 
+             max_sum  =  max(current_sum , max_sum); 
+        }
+        
+        return max_sum ; 
+    }
+    
+    
+    int maxSubarraySumCircular(vector<int>& A) {
+        
+        int max_kadane = KadaneAlgorithm(A); 
+        
+        if(max_kadane < 0) return max_kadane; //For All the Non Negative Elements Of the Subarray. 
+        
+        int max_wrap = 0 ; 
+        
+        for(int i=0 ; i<A.size() ; i++){
+            max_wrap += A[i]; 
+            A[i] = -A[i] ; //Inverting Sign Of Each Element
+        }
+        
+        max_wrap = max_wrap + KadaneAlgorithm(A); 
+        
+       if(max_wrap > max_kadane){
+          return max_wrap ;  
+       }else{
+           return max_kadane ; 
+       }
+    }
+};
+
+
+
